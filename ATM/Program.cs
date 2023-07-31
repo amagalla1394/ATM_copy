528ce65fafe11ce6a79959d07db5493e80fa5fee
amagalla <angelmagallanes18@gmail.com>
2023-07-31 13:09:32 -0500

    create get/set items

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -15,4 +15,47 @@ public class cardHolder
         this.lastName = lastName;
         this.balance = balance;
     }
+
+    public string getNum()
+    {
+        return cardNum;
+    }
+    public int getPin()
+    {
+        return pin;
+    }
+    public string getFirstName()
+    {
+        return firstName;
+    }
+    public string getLastName()
+    {
+        return lastName;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
 }
