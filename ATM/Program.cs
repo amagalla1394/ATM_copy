9c56d47e7edeeac0cabc7ebca9bccb176fa210db
amagalla <angelmagallanes18@gmail.com>
2023-07-31 12:57:55 -0500

    initial steps created with variables and constructor.

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class cardHolder
+{
+    string cardNum;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;            
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+}
